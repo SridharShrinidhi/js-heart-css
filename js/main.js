6328405af7366321e54494198fd1bb7181393ed4
Shrinidhi Sridhar <shrinidhisridhar30@gmail.com>
2018-05-21 14:41:40 -0400

    Add the basic Jquery

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+
+$header.addClass('masthead');
+$h1.removeClass('big-heading');
+$h1.addClass('logo');
+$main.addClass('wrapper');
+$('main p:first-child').addClass('intro');
+$('h2').addClass('slug-head');
+$('main ul').addClass('slug-list');
+$('img').addClass('slug-img');
+$('ul li:first-child').addClass('slug-list-first');
+$('ul li:last-child').addClass('slug-list-last');
+$('dl').addClass('classification');
+$('dt').removeClass('big-label');
+$('ul li:last-child').show();
